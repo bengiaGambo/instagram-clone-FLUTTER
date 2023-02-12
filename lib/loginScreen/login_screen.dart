da5864a845a12cd914f0c43cd426711fd25527e9
Ka-ambo <bengiagambo7@gmail.com>
2023-02-12 07:13:12 +0530

    added default theme to textfied

---
+++ lib/loginScreen/login_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/widgets/my_textfield.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        // backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            Image.asset('assets/instagram_logo.png'),
+            MyTextField(
+              height: 44,
+              isTest: true,
+            ),
+            MyTextField(),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/loginScreen/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final _darkTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.blue,
+  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Instagram Clone',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: _darkTheme,
+      home: const LoginScreen(),
     );
   }
 }
